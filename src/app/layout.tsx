5ef0ff3e7291e266ca143f63de019bfd6861a9bc
Samir Rai <samikhaling14@gmail.com>
2023-08-07 23:23:44 -0400

    Restaurent home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/footer'
+import Notification from '@/components/Notification'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Notification />
+        <Navbar />        
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
---
+++ src/components/CartIcon.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import React from 'react'
+
+export const CartIcon = () => {
+  return (
+    <Link href="/cart" className='flex items-center gap-4'>
+        <div className='relative w-8 h-8 md:w-5 md:h-5'>
+            <Image src="/cart.png" alt='' fill/>
+        </div>
+        <span>Cart (3)</span>
+    </Link>
+  )
+}
